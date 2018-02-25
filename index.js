aece636a2642b5a93f4b9ab01eb4fb987f938dc8
mojaray2k <amen.ra@imaginationeverywhere.info>
2018-02-24 20:33:22 -0500

    updating code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var url = require('url');
 var express = require('express');
+var querystring = require('querystring');
 var authenticator = require('./authenticator');
 var config = require('./config');
 var app = express();
@@ -11,8 +12,8 @@ app.use(require('cookie-parser')());
 app.get('/auth/twitter', authenticator.redirectToTwitterLoginPage);
 
 // This is the callback url that the user is redirected to after signing in
-app.get(url.parse(config.oauth_callback).path, function(req, res) {
-	authenticator.authenticate(req, res, function(err) {
+app.get(url.parse(config.oauth_callback).path, (req, res) => {
+	authenticator.authenticate(req, res, (err) => {
 		if (err) {
 			console.log(err);
 			res.sendStatus(401);
@@ -22,7 +23,64 @@ app.get(url.parse(config.oauth_callback).path, function(req, res) {
 	});
 });
 
+// Tweet
+app.get('/tweet', (req, res) => {
+	if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+		return res.sendStatus(401);
+	}
+
+	authenticator.post('https://api.twitter.com/1.1/statuses/update.json',
+		req.cookies.access_token, req.cookies.access_token_secret,
+		{
+			status: "Hello Twitter REST API"
+		},
+		(error, data) => {
+			if (error) {
+				return res.status(400).send(error);
+			}
+
+			res.send("Tweet successful!");
+		});
+});
+
+// Search
+app.get('/search', (req, res) => {
+	if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+		return res.sendStatus(401);
+	}
+
+	authenticator.get('https://api.twitter.com/1.1/search/tweets.json?' + querystring.stringify({ q: 'French' }),
+		req.cookies.access_token, req.cookies.access_token_secret,
+		(error, data) => {
+			if (error) {
+				return res.status(400).send(error);
+			}
+			res.send(data);
+		});
+});
+
+// List friends
+app.get('/friends', (req, res) => {
+	if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+		return res.sendStatus(401);
+	}
+
+	var url = 'https://api.twitter.com/1.1/friends/list.json';
+	if (req.query.cursor) {
+		url += '?' + querystring.stringify({ cursor: req.query.cursor });
+	}
+
+	authenticator.get(url,
+		req.cookies.access_token, req.cookies.access_token_secret,
+		(error, data) => {
+			if (error) {
+				return res.status(400).send(error);
+			}
+			res.send(data);
+		});
+});
+
 // Start listening for requests
-app.listen(config.port, function() {
+app.listen(config.port, () => {
 	console.log("Listening on port " + config.port);
 });
